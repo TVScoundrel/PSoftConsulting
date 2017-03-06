97224b5b6a0f1178c8ee378eded6fc775fa25438
Tom Van Schoor <tom@tutuka.com>
2017-03-06 14:11:41 +0100

    For now install as dev

--- deploy/work.sh
+++ deploy/work.sh
@@ -25,8 +25,8 @@ else
 fi
 
 # Install dependencies
-npm install --production
-npm prune --production
+npm install --dev
+npm prune --dev
 
 # Restart app
 passenger-config restart-app --ignore-app-not-running --ignore-passenger-not-running $RESTART_ARGS $APP_DIR/code
