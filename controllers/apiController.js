bf7ebd5871669b236a859ce202268b622d77b450
Tom Van Schoor <tom@tutuka.com>
2017-03-06 13:54:11 +0100

    Bug: req not defined
    Why:
    * forgot to pass it in the callback function

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
 
-  app.get('/api/todos/:uname', function () {
+  app.get('/api/todos/:uname', function (req, res) {
     Todos.find({ username: req.params.uname }, function (err, todos) {
       if (err) throw err
 
@@ -15,7 +15,7 @@ module.exports = function (app) {
     })
   })
 
-  app.get('/api/todos/:id', function () {
+  app.get('/api/todos/:id', function (req, res) {
     Todos.findById({ _id: req.params.id }, function (err, todo) {
       if (err) throw err
       res.send(todo)
